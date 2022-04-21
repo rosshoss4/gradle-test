d8141df482cae106e17487a989be33d518bdda0c
Ross Hosseinpour <101831230+rosshoss4@users.noreply.github.com>
2022-04-21 11:14:43 -0700

    Delete GradleTest.java

--- src/test/java/com/roshoss/GradleTest.java
+++
@@ -1,9 +0,0 @@
-
-package com.rosshoss;
-import org.junit.Test;
-public class GradleTest {
-    @Test
-    public void verifyNoExceptionThrown() {
-        GradleTutorial.main(new String[]{});
-    }
-}
