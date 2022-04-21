dae5f5d69b625ccd06f90cc8eded7a043da68701
Ross Hosseinpour <101831230+rosshoss4@users.noreply.github.com>
2022-04-21 10:15:34 -0700

    Update GradleTest.java

--- src/test/java/com/roshoss/GradleTest.java
+++ src/test/java/com/roshoss/GradleTest.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 public class GradleTest {
     @Test
     public void verifyNoExceptionThrown() {
-        GradleTutorial.main(new String[]{});
+        1GradleTutorial.main(new String[]{});
     }
 }
