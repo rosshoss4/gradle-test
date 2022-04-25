bb45166ce445a6d910d773a775b807c2bc2f3c1c
Ross Hosseinpour <101831230+rosshoss4@users.noreply.github.com>
2022-04-25 10:36:12 -0700

    Update TemperatureCalculatorTest.java

--- src/test/java/com/samsung/TemperatureCalculatorTest.java
+++ src/test/java/com/samsung/TemperatureCalculatorTest.java
@@ -8,9 +8,9 @@ public class TemperatureCalculatorTest {
         assertEquals(36.666668f,calculator.toDegree(98),0);
     }
  
-    @Test
-    public void testToFahrenheit(){
-        TemperatureCalculator calculator = new TemperatureCalculator();
-        assertEquals("Test failed for Degree to Fahrenheit",100f,calculator.toFahrenheit(100),0);
-    }
+    //@Test
+    //public void testToFahrenheit(){
+     //   TemperatureCalculator calculator = new TemperatureCalculator();
+      //  assertEquals("Test failed for Degree to Fahrenheit",100f,calculator.toFahrenheit(100),0);
+   // }
 }
